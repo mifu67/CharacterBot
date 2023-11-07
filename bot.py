c8762ff7a416ab63b001e26be7bf1070d5b22641
Michelle Fu <mechllfu@gmail.com>
2023-11-06 23:39:34 -0800

    hopes and dreams

---
+++ bot.py
@@ -0,0 +1,19 @@
+"""
+Nothing here... yet. But there will be!
+Here's the process: 
+Step 1: ask the user which character they'd like to talk to
+Step 2: 
+    - grab the personality and quotes page for the specified character
+    - create our prompts and store them in specific message lists
+    - output a welcome message and get the ball rolling
+Step 3: chat, using LlamaIndex to get the relevant grounding text
+    - model flow: question -> text retrieval -> character answer -> voice rewriting -> output
+Step 4: profit
+"""
+
+def main():
+    print("Welcome to CharacterBot! We're bringing your favorite characters to life from a galaxy far, far away.")
+    print("Nothing to see here... yet. Stay tuned!")
+
+if __name__ == "__main__":
+    main()
