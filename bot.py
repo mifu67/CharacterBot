ca7f88ebe28721eca80b0aa653202ec450f8fa0e
Michelle Fu <mechllfu@gmail.com>
2023-11-08 15:59:49 -0800

    fetched raw text for certain characters

--- bot.py
+++ bot.py
@@ -1,14 +1,5 @@
 """
-Nothing here... yet. But there will be!
-Here's the process: 
-Step 1: ask the user which character they'd like to talk to
-Step 2: 
-    - grab the personality and quotes page for the specified character
-    - create our prompts and store them in specific message lists
-    - output a welcome message and get the ball rolling
-Step 3: chat, using LlamaIndex to get the relevant grounding text
-    - model flow: question -> text retrieval -> character answer -> voice rewriting -> output
-Step 4: profit
+Main file for CharacterBot
 """
 
 def main():
--- fetch_data.py
+++ fetch_data.py
@@ -1,29 +1,42 @@
 """
-Uses the fandom-py library to fetch data necessary for grounding
+Uses the fandom-py library to fetch data necessary for grounding.
 """
 
 import fandom
 import os
 
-def fetch_data(character: str)-> None:
+from typing import List
+
+def fetch_data(characters: List[str])-> None:
     fandom.set_wiki("Wookieepedia")
+    # if this is the first time using characterbot, we need to create a new "raw_data" directory
     try:
-        backstory = fandom.page(character)
-        print("Relevant page found!")
-    except fandom.error.PageError:
-        print("Sorry, it seems like we couldn't find that character in Wookieepedia. Please try again.")
-        return
-    
-    # if this is the first time using characterbot, we need to create a new "data" directory
-    try:
-        os.mkdir("data")
+        os.mkdir("raw_data")
         print("Created data folder.")
     except FileExistsError:
         print("Data directory found, skipping folder creation.")
 
-    # get the filename from the full character name
-    fname = "data/" + ''.join(character.split()).lower() + ".txt"
-    f = open(fname, "w")
-    # write formatted plain text to file
-    print(backstory.plain_text, file=f)
-    f.close()
+    for character in characters:
+        try:
+            backstory = fandom.page(character)
+            print("Relevant page found!")
+        except fandom.error.PageError:
+            print("Sorry, it seems like we couldn't find " + character + " in Wookieepedia. Please try again.")
+            continue
+
+        # get the filename from the full character name
+        fname = "raw_data/" + ''.join(character.split()).lower() + ".txt"
+        f = open(fname, "w")
+        # write formatted plain text to file
+        print(backstory.plain_text, file=f)
+        f.close()
+
+def main():
+    characters = ["Han Solo", "Leia Organa Solo", "Anakin Skywalker", "Obi-Wan Kenobi", "Yoda"]
+    # you'll get a keyerror on this page when using fandom-py 
+    # I changed the get_section function somewhat to get this text; just use the provided file for simplicity
+    characters = ["Luke Skywalker"] 
+    fetch_data(characters)
+
+if __name__ == "__main__":
+    main()
