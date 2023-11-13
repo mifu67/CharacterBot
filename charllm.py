08e1498d7db664b4578cf643e98c737ac81578bc
Adam Zhao <adamzhao@stanford.edu>
2023-11-13 15:39:40 -0800

    creating charllm file

---
+++ charllm.py
@@ -0,0 +1,8 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import json
+
+EXPERIENCE_PATH = 'trainingdata/hansolo/young-han.json'
+PROTECTED_PATH = 'trainingdata/hansolo/young-han-protective.json'
+
