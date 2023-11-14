db195804dbbc9bde45c15a33ac871683dfe04e7a
Adam Zhao <adamzhao@stanford.edu>
2023-11-13 16:11:40 -0800

    hiding api key

--- charllm.py
+++ charllm.py
@@ -2,7 +2,18 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import json
+import together
+from apikey import KEY
 
 EXPERIENCE_PATH = 'trainingdata/hansolo/young-han.json'
 PROTECTED_PATH = 'trainingdata/hansolo/young-han-protective.json'
 
+together.api_key = KEY
+
+model_list = together.Models.list()
+
+print(f"{len(model_list)} models available")
+
+# print the first 10 models on the menu
+model_names = [model_dict['name'] for model_dict in model_list]
+print(model_names[:10])
