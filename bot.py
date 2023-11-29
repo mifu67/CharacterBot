f6269f554b92c35ef69db98b1d6e068fb150170c
Michelle Fu <mechllfu@gmail.com>
2023-11-28 20:41:49 -0800

    some bot changes

--- bot.py
+++ bot.py
@@ -9,13 +9,18 @@ OLD = "mifu67@stanford.edu/llama-2-7b-chat-old-han-second-10--1e-05-2023-11-25-2
 
 
 def chat_loop(model: str) -> None:
-    pass
+    user_turn = ""
+    while user_turn.strip() != 'quit':
+        user_turn = input("What do you want to say? Type 'quit' to stop chatting.")
+    
+
 
 def main():
     print("Welcome to CharacterBot! This time, we're bringing Han Solo to life from a galaxy far, far away.")
     model = input("Choose a model from 'YOUNG', 'REGULAR', or 'OLD': ").strip()
     while model not in MODEL_SET:
         model = input("Unrecognized input. Please choose from 'YOUNG', 'REGULAR', or 'OLD': ").strip()
+    print("--------------------------------------------------")
     chat_loop(model)
 
 if __name__ == "__main__":
